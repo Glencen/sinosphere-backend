5277d49a00ca588e70c50d85b23f48f1159739d7
Dmitry Silantiev <sylantiev21@gmail.com>
2025-10-28 03:19:01 +0300

    Created user profile model

--- sinosphere_backend/users/models.py
+++ sinosphere_backend/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from dictionary.models import Dictionary
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    personal_dictionary = models.OneToOneField(
+        Dictionary,
+        on_delete=models.CASCADE,
+        related_name='user_profile'
+    )
+    
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+    
+    def __str__(self):
+        return f"Профиль: {self.user.username}"
