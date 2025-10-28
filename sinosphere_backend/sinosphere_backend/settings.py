5564ec9a422526592ba6ca15dee40129d9373304
Dmitry Silantiev <sylantiev21@gmail.com>
2025-10-28 03:25:29 +0300

    Switched database to MySQL in config

--- sinosphere_backend/sinosphere_backend/settings.py
+++ sinosphere_backend/sinosphere_backend/settings.py
@@ -98,8 +98,16 @@ WSGI_APPLICATION = 'sinosphere_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sinosphere',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
