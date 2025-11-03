23410420b4403d8c9b2c36200c52e3d7be31a28e
Dmitry Silantiev <sylantiev21@gmail.com>
2025-11-03 17:18:36 +0300

    Added save_model method to admin dict registration

--- sinosphere_backend/dictionary/admin.py
+++ sinosphere_backend/dictionary/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 from .models import Word, Dictionary, DictionaryEntry
 
 @admin.register(Word)
@@ -12,6 +13,13 @@ class DictionaryAdmin(admin.ModelAdmin):
     list_display = ('name', 'dictionary_type')
     list_filter = ('dictionary_type',)
     search_fields = ('name',)
+    readonly_fields = ('dictionary_type',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+        except ValidationError as e:
+            self.message_user(request, str(e), level='ERROR')
 
 @admin.register(DictionaryEntry)
 class DictionaryEntryAdmin(admin.ModelAdmin):
