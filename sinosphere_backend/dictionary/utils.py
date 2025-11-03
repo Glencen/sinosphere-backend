ca7652950587bfe62b0f9818446a93d375218d78
Dmitry Silantiev <sylantiev21@gmail.com>
2025-11-03 17:16:54 +0300

    Added a get/create function to utils for a global dictionary

--- sinosphere_backend/dictionary/utils.py
+++ sinosphere_backend/dictionary/utils.py
@@ -1,7 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from .models import Dictionary, Word, DictionaryEntry
 
+def get_or_create_global_dictionary():
+    try:
+        global_dict = Dictionary.objects.get(dictionary_type='global')
+    except Dictionary.DoesNotExist:
+        global_dict = Dictionary.objects.create(
+            name='Глобальный словарь',
+            dictionary_type='global'
+        )
+    return global_dict
+
 def add_to_global_dictionary(word_data):
-    global_dict = Dictionary.objects.get(dictionary_type='global')
+    global_dict = get_or_create_global_dictionary()
     word = Word.objects.create(**word_data)
     DictionaryEntry.objects.create(
         dictionary=global_dict,
@@ -13,7 +24,7 @@ def get_user_personal_dictionary(user):
     return user.profile.personal_dictionary
 
 def get_global_dictionary():
-    return Dictionary.objects.get(dictionary_type='global')
+    return get_or_create_global_dictionary()
 
 def is_word_in_personal_dictionary(user, word):
     personal_dict = get_user_personal_dictionary(user)
