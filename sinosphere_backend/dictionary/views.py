5029e137f0cc492ed41e0e3e565029f82cf42cd3
Dmitry Silantiev <sylantiev21@gmail.com>
2025-12-26 05:15:08 +0300

    Removed spaces

--- sinosphere_backend/dictionary/views.py
+++ sinosphere_backend/dictionary/views.py
@@ -15,7 +15,6 @@ class WordListCreateView(APIView):
     """
     API для получения списка слов и создания нового слова
     """
-    
     def get(self, request):
         words = Word.objects.all()
         serializer = WordSerializer(words, many=True)
@@ -33,7 +32,6 @@ class WordDetailView(APIView):
     """
     API для получения, обновления и удаления конкретного слова
     """
-    
     def get(self, request, pk):
         word = get_object_or_404(Word, pk=pk)
         serializer = WordSerializer(word)
@@ -64,7 +62,6 @@ class WordCompositionListCreateView(APIView):
     """
     API для управления композициями слов
     """
-    
     def get(self, request):
         compositions = WordComposition.objects.all()
         serializer = WordCompositionSerializer(compositions, many=True)
@@ -85,7 +82,6 @@ class BulkWordCompositionCreateView(APIView):
     """
     API для массового создания композиций слов
     """
-    
     def post(self, request):
         serializer = BulkWordCompositionSerializer(data=request.data)
         if serializer.is_valid():
@@ -100,7 +96,6 @@ class WordCompositionDetailView(APIView):
     """
     API для управления конкретной композицией слова
     """
-    
     def get(self, request, pk):
         composition = get_object_or_404(WordComposition, pk=pk)
         serializer = WordCompositionSerializer(composition)
@@ -124,7 +119,6 @@ class WordTagListCreateView(APIView):
     """
     API для управления тегами слов
     """
-    
     def get(self, request):
         word_tags = WordTag.objects.all()
         serializer = WordTagSerializer(word_tags, many=True)
@@ -142,7 +136,6 @@ class WordTagDetailView(APIView):
     """
     API для управления конкретным тегом слова
     """
-    
     def get(self, request, pk):
         word_tag = get_object_or_404(WordTag, pk=pk)
         serializer = WordTagSerializer(word_tag)
@@ -158,7 +151,6 @@ class WordPartOfSpeechListCreateView(APIView):
     """
     API для управления частями речи слов
     """
-    
     def get(self, request):
         word_pos = WordPartOfSpeech.objects.all()
         serializer = WordPartOfSpeechSerializer(word_pos, many=True)
@@ -176,7 +168,6 @@ class WordPartOfSpeechDetailView(APIView):
     """
     API для управления конкретной частью речи слова
     """
-    
     def get(self, request, pk):
         word_pos = get_object_or_404(WordPartOfSpeech, pk=pk)
         serializer = WordPartOfSpeechSerializer(word_pos)
@@ -192,7 +183,6 @@ class TagListView(APIView):
     """
     API для получения списка всех тегов и добавления нового
     """
-    
     def get(self, request):
         tags = Tag.objects.all()
         serializer = TagSerializer(tags, many=True)
@@ -209,7 +199,6 @@ class TagDetailView(APIView):
     """
     API для получения, обновления и удаления конкретного тега
     """
-    
     def get(self, request, name):
         tag = get_object_or_404(Tag, name=name)
         serializer = TagSerializer(tag)
@@ -241,7 +230,6 @@ class PartOfSpeechListView(APIView):
     """
     API для получения списка всех частей речи и добавления новой
     """
-    
     def get(self, request):
         parts_of_speech = PartOfSpeech.objects.all()
         serializer = PartOfSpeechSerializer(parts_of_speech, many=True)
@@ -258,7 +246,6 @@ class PartOfSpeechDetailView(APIView):
     """
     API для получения, обновления и удаления конкретной части речи
     """
-    
     def get(self, request, name):
         part_of_speech = get_object_or_404(PartOfSpeech, name=name)
         serializer = PartOfSpeechSerializer(part_of_speech)
@@ -310,20 +297,13 @@ class WordSearchView(APIView):
         if difficulty:
             queryset = queryset.filter(difficulty=difficulty)
         
-        # Фильтрация по тегу
         tag = query_params.get('tag')
         if tag:
             queryset = queryset.filter(tags__tag__name=tag)
         
-        # Фильтрация по части речи
         part_of_speech = query_params.get('part_of_speech')
         if part_of_speech:
             queryset = queryset.filter(parts_of_speech__part_of_speech__name=part_of_speech)
-        
-        # Поиск по переводу
-        translation = query_params.get('translation')
-        if translation:
-            queryset = queryset.filter(translation__icontains=translation)
         
         serializer = WordSerializer(queryset.distinct(), many=True)
         return Response(serializer.data)
@@ -333,7 +313,6 @@ class WordByDifficultyView(APIView):
     """
     API для получения слов по уровню сложности HSK
     """
-    
     def get(self, request, difficulty):
         words = Word.objects.filter(difficulty=difficulty)
         serializer = WordSerializer(words, many=True)
