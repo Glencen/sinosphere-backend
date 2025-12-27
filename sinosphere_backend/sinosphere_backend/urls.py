bd0efd61ede42cff1ba23dbe762197038c0e81b4
Dmitry Silantiev <sylantiev21@gmail.com>
2025-12-27 07:19:52 +0300

    Added 'learning' module to settings

--- sinosphere_backend/sinosphere_backend/urls.py
+++ sinosphere_backend/sinosphere_backend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/dictionary/', include('dictionary.urls')),
+    path('api/learning/', include('learning.urls')),
 ]
