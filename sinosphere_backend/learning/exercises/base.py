7aef0095a2792259385beeade8f525a1640cd541
Dmitry Silantiev <sylantiev21@gmail.com>
2026-08-02 04:34:12 +0300

    Created a base exercise handler class

---
+++ sinosphere_backend/learning/exercises/base.py
@@ -0,0 +1,26 @@
+﻿from abc import ABC, abstractmethod
+
+from learning.models import ExerciseAttempt
+
+from .dto import ExerciseGenerationContext, GeneratedExercise, GradeResult
+
+
+class ExerciseHandler(ABC):
+    kind: str
+    version: int
+
+    @abstractmethod
+    def validate_config(self, config: dict) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate(self, context: ExerciseGenerationContext) -> GeneratedExercise:
+        raise NotImplementedError
+
+    @abstractmethod
+    def validate_answer(self, attempt: ExerciseAttempt, answer: dict) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def grade(self, attempt: ExerciseAttempt, answer: dict) -> GradeResult:
+        raise NotImplementedError
