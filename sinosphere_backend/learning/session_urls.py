b490443b222b518feded11e6467e1b5b8a478ce3
Dmitry Silantiev <sylantiev21@gmail.com>
2026-08-02 05:32:37 +0300

    Created urls for sessions

---
+++ sinosphere_backend/learning/session_urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('practice-sessions/', views.PracticeSessionCreateView.as_view(), name='practice-session-create-api'),
+    path('practice-sessions/<int:session_id>/', views.PracticeSessionApiDetailView.as_view(), name='practice-session-detail-api'),
+    path('practice-sessions/<int:session_id>/current-exercise/', views.PracticeSessionCurrentExerciseView.as_view(), name='practice-session-current-exercise-api'),
+    path('exercise-attempts/<int:attempt_id>/submit/', views.ExerciseAttemptSubmitView.as_view(), name='exercise-attempt-submit-api'),
+]
--- sinosphere_backend/sinosphere_backend/urls.py
+++ sinosphere_backend/sinosphere_backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('learning.session_urls')),
     path('api/', include('users.urls')),
     path('api/dictionary/', include('dictionary.urls')),
     path('api/learning/', include('learning.urls')),
