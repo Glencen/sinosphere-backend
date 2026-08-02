c4a764fd744071f23e27ebbfdb5a5cc556993a61
Dmitry Silantiev <sylantiev21@gmail.com>
2026-08-02 05:30:36 +0300

    Registered new handlers

--- sinosphere_backend/learning/exercises/__init__.py
+++ sinosphere_backend/learning/exercises/__init__.py
@@ -1,6 +1,10 @@
 ﻿from .registry import registry
+from .handlers.matching import MatchingHandler
 from .handlers.multiple_choice import MultipleChoiceHandler
+from .handlers.translation_ru import TranslationRuHandler
 
 registry.register(MultipleChoiceHandler())
+registry.register(TranslationRuHandler())
+registry.register(MatchingHandler())
 
 __all__ = ['registry']
